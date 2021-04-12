d1e091da7b86efda4e5c78b6ffee9c7d32a952db
jp-martinezc <jp.martinezc@users.noreply.github.com>
2021-04-12 15:13:48 -0500

    ayuda

--- frontback/app-back/routes/offers.js
+++ frontback/app-back/routes/offers.js
@@ -11,4 +11,27 @@ router.get('/', function (req, res, next) {
     })
 });
 
+router.post('/offers', function(req, res){
+    res.send({
+        type: 'POST',
+        name: req.body.name,
+        roll: req.body.roll
+    });
+});
+
+router.put('/offers/:id',function(req,res,next){
+    Job.findOneAndUpdate({_id: req.params.id},req.body).then(function(job){
+        Job.findOne({_id: req.params.id}).then(function(job){
+            res.send(job);
+        });
+    });
+});
+
+
+router.delete('/offers/:id',function(req,res,next){
+    Job.findOneAndDelete({_id: req.params.id}).then(function(job){
+        res.send(job);
+    });
+});
+
 module.exports = router;
